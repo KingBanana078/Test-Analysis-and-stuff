2b69b583c2b9cfbe879734ca3ba82081b70eb307
VlasblomDaan <daan.vlasblom6@gmail.com>
2025-03-10 09:47:03 +0100

    update

--- TestDataTester.py
+++ TestDataTester.py
@@ -2,7 +2,10 @@ import csv
 import numpy as np
 import matplotlib.pyplot  as plt
 from scipy.spatial import Delaunay
+import pandas as pd
 
+pd.read_csv('NewTestData(2).csv', comment = ";" )
+ 
 with open('NewTestData(2).csv') as f:
     reader = csv.reader(f)
     hot_spots_data = list(reader)
