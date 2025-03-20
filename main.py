97ff23a5cfb4dc0b3ebbe3d7bb4c8f0e79973728
VlasblomDaan <daan.vlasblom6@gmail.com>
2025-03-20 11:55:59 +0100

    conversion to spherical of the centroids is wrong

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial import SphericalVoronoi, geometric_slerp
 from scipy.interpolate import Rbf
 import math
+import healpix as hp
 
 
 from scipy.interpolate import Rbf, RBFInterpolator
@@ -106,29 +107,48 @@ def compute_density(sv):
     # Define density as inverse of area (higher area = lower density)
     densities = 1 /  areas
 
+
+    centroids2 = np.array([np.mean(sv.vertices[region], axis=0) for region in sv.regions])
+    centroids2 /= np.linalg.norm(centroids, axis=1)[:, np.newaxis]
+
+    """ this is computed wrongly"""
+    for item in centroids2:
+        item[2] = math.acos(item[2])
+        item [1] = math.acos(item [0] / math.sin(item[2]))
+        item[0] = 1
+    
+    print(centroids[4], centroids2[4])
+
     # Create interpolation function (RBF) using centroids
     rbf = Rbf(centroids[:, 0], centroids[:, 1], centroids[:, 2], densities, function='linear')
+    rbf2 = Rbf(centroids2[:, 0], centroids2[:, 1], centroids2[:, 2], densities, function='linear')
 
-    return rbf
+    return rbf, rbf2
 
-def plot_density(rbf):
+def plot_density(rbf, rbf2):
     """Plots a 2D density map from the RBF interpolation."""
     num_grid = 360
-    grid_theta = np.linspace(0, np.pi*2, num_grid)
-    grid_phi = np.linspace(0, np.pi, num_grid)
+    grid_theta = np.linspace(-np.pi, np.pi, num_grid)
+    grid_phi = np.linspace(-np.pi/2, np.pi/2, num_grid)
+    onesarray = np.ones((len(grid_theta), len(grid_theta)))
     phi_grid, theta_grid = np.meshgrid(grid_phi, grid_theta)
+
+    grid_theta2 = np.linspace(-np.pi, np.pi, num_grid)
+    grid_phi2 = np.linspace(-np.pi/2, np.pi/2, num_grid)
+    phi_grid2, theta_grid2 = np.meshgrid(grid_phi2, grid_theta2)
 
     # Convert grid points to Cartesian coordinates
     x_grid = np.cos(theta_grid) * np.sin(phi_grid)
     y_grid = np.sin(theta_grid) * np.sin(phi_grid)
     z_grid = np.cos(phi_grid)
-
     # Interpolate density on grid
     density_grid = rbf(x_grid, y_grid, z_grid)
 
+    density_grid2 = rbf2(onesarray, theta_grid2, phi_grid2)
     # Plot the density map
     plt.figure(figsize=(10, 5))
-    plt.pcolormesh(np.linspace(-180, 180, num_grid), np.linspace(-90, 90, num_grid), density_grid, shading='auto', cmap='viridis')
+    #plt.subplot(projection = 'mollweide')
+    plt.pcolormesh(np.linspace(-180,180, num_grid), np.linspace(-90, 90, num_grid), density_grid2, shading='auto', cmap='viridis')
     plt.colorbar(label='Density')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
@@ -150,8 +170,8 @@ def main():
     plot_voronoi(sv, points)
 
     # Compute and plot density
-    rbf = compute_density(sv)
-    plot_density(rbf)
+    rbf, rbf2 = compute_density(sv)
+    plot_density(rbf, rbf2)
 
 if __name__ == "__main__":
     main()
