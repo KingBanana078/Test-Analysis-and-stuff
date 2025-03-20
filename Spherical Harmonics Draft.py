5e951734ededeecb6721b709dc5ef9961abb9a90
Vee <venlaniinivaara@gmail.com>
2025-03-20 09:31:27 +0100

    Update

--- Spherical Harmonics Draft.py
+++ Spherical Harmonics Draft.py
@@ -11,28 +11,29 @@ with open(r"Positiondata.csv")as csvfile:
 
 hot_spots_data = np.array(hot_spots_data, dtype=float)
 
-# theta = hot_spots_data[:, 0]
-# latitude = hot_spots_data[:, 1]
-# phi = math.pi/2*latitude
+theta_degrees = hot_spots_data[:, 0]
+latitude_degrees = hot_spots_data[:, 1]
+theta = np.radians(theta_degrees)
+latitude = np.radians(latitude_degrees)
+phi = math.pi/2 - latitude
+phi, theta = np.meshgrid(phi, theta)
 
-# l = np.arange(0, 343, 1)
-# m = np.arange(0, 343, 1)
-
+m, l = 20 , 25
 
 # sph_harm(l, m, theta, phi)
 
+# print(theta, phi)
 # print(sph_harm(l, m, theta, phi))
 
-phi = np.linspace(0, np.pi, 100)
-theta = np.linspace(0, 2*np.pi, 100)
-phi, theta = np.meshgrid(phi, theta)
+# phi = np.linspace(0, np.pi, 100)
+# theta = np.linspace(0, 2*np.pi, 100)
+
 
 # The Cartesian Coordinates of a Unit Sphere
 x = np.sin(phi) * np.cos(theta)
 y = np.sin(phi) * np.sin(theta)
 z = np.cos(phi)
 
-m, l = 2, 3
 
 # Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
 fcolors = sph_harm(m, l, theta, phi).real
