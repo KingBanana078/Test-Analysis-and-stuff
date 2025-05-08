e3da727644d299098e34bf9d4e4b9f4493252283
JSvahn1 <julia.svahn0807@gmail.com>
2025-05-08 11:49:39 +0200

    Update Correlation_overlay.py

--- Correlation_overlay.py
+++ Correlation_overlay.py
@@ -16,7 +16,7 @@ ssim_score = compare_ssim(imageA, imageB)
 print(f"SSIM Score: {ssim_score}")"""
 
 
-import io
+"""import io
 import numpy as np
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
@@ -46,4 +46,42 @@ imageB = np.array(img2)
 
 # Compute SSIM
 score, diff = ssim(imageA, imageB, full=True)
+print(f"SSIM Score: {score}")"""
+
+import io
+import numpy as np
+from PIL import Image
+from skimage.metrics import structural_similarity as ssim
+
+# Load Deep Mantle
+with open(r"C:\Users\julia\OneDrive\Dokument\deep_mantle.jpg", "rb") as file:
+    deep_mantle = Image.open(io.BytesIO(file.read()))
+
+# Load Asthenosphere
+with open(r"C:\Users\julia\OneDrive\Dokument\asthentosphere.jpg", "rb") as file:
+    asthenosphere = Image.open(io.BytesIO(file.read()))
+
+# Load Magma Ocean
+with open(r"C:\Users\julia\OneDrive\Dokument\magma_ocean.jpg", "rb") as file:
+    magma_ocean = Image.open(io.BytesIO(file.read()))
+
+with open(r"C:\Users\julia\OneDrive\Pictures\Screenshots\test2.png", "rb") as file:
+    test = Image.open(io.BytesIO(file.read()))
+
+# Convert to grayscale
+img1 = test.convert('L')
+img2 = asthenosphere.convert('L')
+
+
+# Resize to match dimensions
+img2 = img2.resize(img1.size)
+
+
+# Convert to NumPy arrays
+imageA = np.array(img1)
+imageB = np.array(img2)
+
+# Compute SSIM
+score, diff = ssim(imageA, imageB, full=True)
 print(f"SSIM Score: {score}")
+
